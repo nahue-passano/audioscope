326e7a23039ab5b663e0e7a14dbec01d363d4688
Nahuel Passano <passano43632@estudiantes.untref.edu.ar>
2024-12-01 18:45:35 -0300

    update: MVP release

--- AudioPlugin/Source/PluginEditor.cpp
+++ AudioPlugin/Source/PluginEditor.cpp
@@ -5,15 +5,14 @@
 AudioScopeAudioProcessorEditor::AudioScopeAudioProcessorEditor(AudioScopeAudioProcessor& p)
     : AudioProcessorEditor(&p), audioProcessor(p), Timer()
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
     setSize(1280, 720);
 
     // scope rectangle
     rectScope = Rectangle(0+3, 3, 1280-6, 500-3);
 
+    // ### Vertical axis
     // Vertical Scale
-    rectVerticalScale = Rectangle(100, 620, 100, 100);
+    rectVerticalScale = Rectangle(350, 600, 100, 100);
     addAndMakeVisible(verticalScale);
     verticalScale.setRange(0, 100, 10);
     verticalScale.setValue(10);
@@ -23,7 +22,7 @@ AudioScopeAudioProcessorEditor::AudioScopeAudioProcessorEditor(AudioScopeAudioPr
     verticalScale.setBounds(rectVerticalScale.toNearestInt());
 
     // Vertical Position
-    rectVerticalPosition = Rectangle(200, 620, 100, 100);
+    rectVerticalPosition = Rectangle(450, 600, 100, 100);
     addAndMakeVisible(verticalPosition);
     verticalPosition.setRange(-200, 200, 10);
     verticalPosition.setValue(0);
@@ -32,12 +31,68 @@ AudioScopeAudioProcessorEditor::AudioScopeAudioProcessorEditor(AudioScopeAudioPr
     verticalPosition.setColour(juce::Slider::textBoxOutlineColourId, juce::Colour(0));
     verticalPosition.setBounds(rectVerticalPosition.toNearestInt());
 
+    // ### Horizontal axis
+    // Horizontal Scale
+    rectHorizontalScale = Rectangle(550, 600, 100, 100);
+    addAndMakeVisible(horizontalScale);
+    horizontalScale.setRange(0, 100, 10);
+    horizontalScale.setValue(10);
+    horizontalScale.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    horizontalScale.setTextBoxStyle(juce::Slider::TextBoxBelow, true, rectHorizontalScale.getWidth(), 20);
+    horizontalScale.setColour(juce::Slider::textBoxOutlineColourId, juce::Colour(0));
+    horizontalScale.setBounds(rectHorizontalScale.toNearestInt());
+
+    // Horizontal Position
+    rectHorizontalPosition = Rectangle(650, 600, 100, 100);
+    addAndMakeVisible(horizontalPosition);
+    horizontalPosition.setRange(-200, 200, 10);
+    horizontalPosition.setValue(0);
+    horizontalPosition.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    horizontalPosition.setTextBoxStyle(juce::Slider::TextBoxBelow, true, rectHorizontalPosition.getWidth(), 20);
+    horizontalPosition.setColour(juce::Slider::textBoxOutlineColourId, juce::Colour(0));
+    horizontalPosition.setBounds(rectHorizontalPosition.toNearestInt());
+
+    // Mode Group
+    rectModeGroup = juce::Rectangle<int>(50, 550, 100, 100);
+    addAndMakeVisible(acMode);
+    acMode.setButtonText("AC");
+    acMode.setBounds(rectModeGroup.getX() + 20, rectModeGroup.getY() + 10, 80, 30);
+
+    addAndMakeVisible(dcMode);
+    dcMode.setButtonText("DC");
+    dcMode.setBounds(rectModeGroup.getX() + 20, rectModeGroup.getY() + 40, 80, 30);
+
+    acMode.setClickingTogglesState(true);
+    dcMode.setClickingTogglesState(true);
+
+    // Range Group
+    rectRangeGroup = juce::Rectangle<int>(150, 550, 200, 100);
+
+    addAndMakeVisible(range1Toggle);
+    range1Toggle.setButtonText("10 mV - 100 mV");
+    range1Toggle.setBounds(rectRangeGroup.getX() + 20, rectRangeGroup.getY() + 10, 150, 30);
+
+    addAndMakeVisible(range2Toggle);
+    range2Toggle.setButtonText("0.1 V - 1 V");
+    range2Toggle.setBounds(rectRangeGroup.getX() + 20, rectRangeGroup.getY() + 40, 150, 30);
+
+    addAndMakeVisible(range3Toggle);
+    range3Toggle.setButtonText("1 V - 10 V");
+    range3Toggle.setBounds(rectRangeGroup.getX() + 20, rectRangeGroup.getY() + 70, 150, 30);
+
+    addAndMakeVisible(range4Toggle);
+    range4Toggle.setButtonText("10 V - 100 V");
+    range4Toggle.setBounds(rectRangeGroup.getX() + 20, rectRangeGroup.getY() + 100, 150, 30);
+
+    range1Toggle.setClickingTogglesState(true);
+    range2Toggle.setClickingTogglesState(true);
+    range3Toggle.setClickingTogglesState(true);
+    range4Toggle.setClickingTogglesState(true);
+
     startTimer(1000 / REFRESH_RATE);
 }
 
-AudioScopeAudioProcessorEditor::~AudioScopeAudioProcessorEditor()
-{
-}
+AudioScopeAudioProcessorEditor::~AudioScopeAudioProcessorEditor() {}
 
 //==============================================================================
 void AudioScopeAudioProcessorEditor::paint(juce::Graphics& g)
@@ -54,7 +109,7 @@ void AudioScopeAudioProcessorEditor::paint(juce::Graphics& g)
     // Dashed horizontal line for VerticalPosition
     juce::Line<float> dashedLine(rectScope.getX(), v_mid - verticalPosition.getValue(), rectScope.getRight(), v_mid - verticalPosition.getValue()); // From (10, 50) to (200, 50)
     float dashLengths[] = { 5.0f, 5.0f }; // 5px dash, 3px space
-    g.drawDashedLine(dashedLine, dashLengths, 2, 1.0f);
+    g.drawDashedLine(dashedLine, dashLengths, 2, 1.5f);
 
     g.setColour(juce::Colours::white);
 
@@ -65,7 +120,7 @@ void AudioScopeAudioProcessorEditor::paint(juce::Graphics& g)
             rectScope.getHeight(),
             v_mid + (audioProcessor.vBuffer.getSample(channel, vBufferIndex) * verticalScale.getValue() * 100.0f) - verticalPosition.getValue()
         );
-        g.drawLine(x0, y0, x, y1, 1.0f);
+        g.drawLine(x0, y0, x, y1, 2.0f);
         x0 = x;
         y0 = y1;
         vBufferIndex = (vBufferIndex + vBufferIncrement) % audioProcessor.vBuffer.getNumSamples();
@@ -73,22 +128,27 @@ void AudioScopeAudioProcessorEditor::paint(juce::Graphics& g)
     g.setColour(juce::Colour (88,121,188));
     g.drawRoundedRectangle(rectScope, 10, 3);
 
-    g.setFont(20);
-    g.drawText("Scale", 100, 600, 100, 20, juce::Justification::centred, false);
-    g.drawText("Position", 200, 600, 100, 20, juce::Justification::centred, false);
+    g.setFont(22);
+    // Vertical labels
+    g.drawText("Vertical", 400, 545, 100, 20, juce::Justification::centred, false);
+    g.drawText("Scale", 350, 580, 100, 20, juce::Justification::centred, false);
+    g.drawText("Position", 450, 580, 100, 20, juce::Justification::centred, false);
+
+    // Horizontal labels
+    g.drawText("Horizontal", 600, 545, 100, 20, juce::Justification::centred, false);
+    g.drawText("Scale", 550, 580, 100, 20, juce::Justification::centred, false);
+    g.drawText("Position", 650, 580, 100, 20, juce::Justification::centred, false);
+
+    // Mode label
+    g.drawText("Mode", rectModeGroup.getX(), rectModeGroup.getY() - 20, rectModeGroup.getWidth(), 20, juce::Justification::centred, false);
+
+    // Range label
+    g.drawText("Range", rectRangeGroup.getX(), rectRangeGroup.getY() - 20, rectRangeGroup.getWidth(), 20, juce::Justification::centred, false);
 }
 
-void AudioScopeAudioProcessorEditor::resized()
-{
-
-}
+void AudioScopeAudioProcessorEditor::resized() {}
 
 void AudioScopeAudioProcessorEditor::timerCallback()
 {
     repaint();
-}
-
-void sliderValueChanged(juce::Slider* slider)
-{
-
 }
--- AudioPlugin/Source/PluginEditor.h
+++ AudioPlugin/Source/PluginEditor.h
@@ -9,7 +9,7 @@ using Rectangle = juce::Rectangle<float>;
 /**
 */
 class AudioScopeAudioProcessorEditor : public juce::AudioProcessorEditor,
-    public juce::Timer
+                                       public juce::Timer
 {
 public:
     AudioScopeAudioProcessorEditor(AudioScopeAudioProcessor&);
@@ -23,13 +23,28 @@ public:
 private:
     AudioScopeAudioProcessor& audioProcessor;
 
-    int const REFRESH_RATE{ 30 };
+    int const REFRESH_RATE{ 10 };
 
     juce::Slider verticalScale;
     juce::Slider verticalPosition;
+    juce::Slider horizontalScale;
+    juce::Slider horizontalPosition;
+    juce::ToggleButton acMode;
+    juce::ToggleButton dcMode;
+    juce::ToggleButton range1Toggle;
+    juce::ToggleButton range2Toggle;
+    juce::ToggleButton range3Toggle;
+    juce::ToggleButton range4Toggle;
+
     Rectangle rectVerticalScale;
     Rectangle rectVerticalPosition;
+    Rectangle rectHorizontalScale;
+    Rectangle rectHorizontalPosition;
     Rectangle rectScope;
+    juce::Rectangle<int> rectModeGroup;
+    juce::Rectangle<int> rectRangeGroup;
+
+    juce::Button::Listener* toggleGroupListener{ nullptr };
 
     int vBufferIndex{ 0 };
     int vBufferIncrement{ 10 };
